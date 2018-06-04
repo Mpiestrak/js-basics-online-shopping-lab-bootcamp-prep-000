c9d7d2681b0d6815d05b57a63808da17f5ab1e90
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-04 22:47:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }else{
     return `Your total cost is $${newTotal}, which will be charged to the card ${cardNumber}.`
-    cart = []
+    cart.length = 0
     return 
   }
 }
