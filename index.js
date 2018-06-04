79306a83b083c05ff3c25cb747642ce5ace87cd5
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-04 21:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,39 @@ function setCart(c) {
   return cart;
 }
 
+
+
 function addToCart(item) {
- // write your code here
+  
+ itemList = {itemName: item, itemPrice: Math.floor(Math.random()*100)}
+ Object.assign({}, itemList, {[itemName]: item}, {[itemPrice]: Math.floor(Math.random()*100)})
+ cart.push(itemList)
+ 
+ return `${item} has been added to your cart.`
 }
 
+
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."}
+    const itemsAndPrices = []
+     for(let i = 0; i<cart.length; i++){
+        let itemAndPrice = cart[i];
+        if(cart.length === 1){
+        let item = Object.values(itemAndPrice)[0]
+        let price = Object.values(itemAndPrice)[1]
+        itemsAndPrices.push(`${item} at \$${price}`)
+              return `In your cart, you have ${itemsAndPrices.join(`,`)}.`
+     }
+     }
 }
 
 function total() {
-  // write your code here
+ var myTotal = 0
+for(var i = 0, len = cart.length; i<len; i++) {
+  myTotal+= cart[i][1];
+}
+return myTotal;
 }
 
 function removeFromCart(item) {
